ae8a55ae4d982cdc5e1db507d3be5fb82f966117
Xriz-San <xrizsanz@gmail.com>
2020-01-29 18:34:19 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { Router } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
+
 
 import { AppComponent }            from './app.component';
 import { PageNotFoundComponent }   from './page-not-found/page-not-found.component';
